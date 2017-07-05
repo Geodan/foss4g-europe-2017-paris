adffd37a5e1b752bf2928dd918dc079760d508f3
Anne Blankert <anneb@METIS.geodan.nl>
2017-07-05 21:15:15 +0200

    removed ^M?

--- simplifygadm/simplifyboxed.sql
+++ simplifygadm/simplifyboxed.sql
@@ -7,11 +7,11 @@ with boxes as (select a.objectid inid, -999 id, ST_ExteriorRing(ST_MakeEnvelope(
 ),
 entities as (select a.objectid inid, b.objectid id, b.wkb_geometry
     from gadm.adm1 a, gadm.adm1 b
-      where st_intersects(a.wkb_geometry,b.wkb_geometry) -- a.unregion1='Western Europe'),      
---     and a.objectid in (2024, 2017)
+      where st_intersects(a.wkb_geometry,b.wkb_geometry) -- a.unregion1='Western Europe'),
+--     and a.objectid > 80 and a.objectid < 160
 )
-,combined as (select * from boxes 
- --where inid in (2024, 2017) 
+,combined as (select * from boxes
+  -- where inid >= 2017 and inid <= 2024
   union all select * from entities
 )
 ,properties as (select inid, id from combined)
@@ -20,31 +20,32 @@ entities as (select a.objectid inid, b.objectid id, b.wkb_geometry
 ,features AS (
     select '{"type": "Feature"}'::JSONB ||
        jsonb_set('{}'::JSONB,'{properties}',row_to_json(p)::JSONB) ||
-	   jsonb_set('{}'::JSONB,'{geometry}',geom::JSONB)
-	as feat
-	 from  properties p  INNER JOIN geometry g USING(inid, id)	
+           jsonb_set('{}'::JSONB,'{geometry}',geom::JSONB)
+        as feat
+         from  properties p  INNER JOIN geometry g USING(inid, id)
 )
--- select feat->'properties'->>'inid', feat->'properties'->>'id', feat->'geometry' from features;
+--select feat->'properties'->>'inid', feat->'properties'->>'id', feat->'geometry' from features;
 ,topojson AS (
     select plv8.d3_totopojson('{"type": "FeatureCollection"}'::JSONB ||
-	jsonb_set('{}'::JSONB, '{features}',jsonb_agg(feat))
-	,1e7) AS topojson
+        jsonb_set('{}'::JSONB, '{features}',jsonb_agg(feat))
+        ,1e7) AS topojson
       FROM features
         GROUP BY feat->'properties'->>'inid'
 )
 -- select * from topojson;
 ,simplified AS (
     SELECT plv8.d3_SimplifyTopology(topojson,0.0001) as topojson
-	    FROM topojson
+            FROM topojson
 )
---select * from simplified;
+-- select * from simplified;
 ,restoredfeatures AS (
     SELECT  plv8.d3_topologytofeatures(topojson) AS feat
-	FROM simplified
+        FROM simplified
 )
 SELECT
     feat->'properties'->>'id' as id_0,
     feat->'properties'->>'inid' as objectid,
     ST_SETSRID(ST_GeomFromGeoJSON((feat->'geometry')::text), 4326) geom
-	FROM restoredfeatures
-	WHERE feat->'properties'->>'id' = feat->'properties'->>'inid';
+        FROM restoredfeatures
+        WHERE feat->'properties'->>'id' = feat->'properties'->>'inid'
+        ;
